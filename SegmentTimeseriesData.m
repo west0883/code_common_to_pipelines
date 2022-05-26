d13fd4adee8dc7833c499e3d232c6be557d12fb1
Sarah West <west0883@umn.edu>
2022-05-26 09:26:43 -0500

    Compatable with RunAnalysis, allows for using cells.

--- SegmentTimeseriesData.m
+++ SegmentTimeseriesData.m
@@ -6,153 +6,70 @@
 % Is general, so you can use with all kinds of timeseries data. 
 
 
-function [] = SegmentTimeseriesData(periods_all, parameters)
-    
-    % Give parameters easier names. 
-    dir_in_data_base = parameters.dir_in_data_base;
-    input_data_name = parameters.input_data_name;
-    input_data_variable = parameters.input_data_variable;
-    dir_in_segment_base = parameters.dir_in_segment_base;
-    input_segment_name = parameters.input_segment_name;
-    input_segment_variable = parameters.input_segment_variable;
-    dir_out_base = parameters.dir_out_base;
-    segmentDim = parameters.segmentDim;
-    concatDim = parameters.concatDim;
-    output_filename = parameters.output_filename;
-    output_variable = parameters.output_variable;
-    mice_all = parameters.mice_all; 
-    digitNumber = parameters.digitNumber;
-   
-    % Tell user where data is being saved
-    disp(['Data saved in '  parameters.dir_out_base{1}]); 
+function [parameters] = SegmentTimeseriesData(parameters)
+        
+        message = ['Segmenting '];
+         for dispi = 1:numel(parameters.values)/2
+            message = [message ', ' parameters.values{dispi}];
+         end
+         disp(message); 
+        % If parameters.time_ranges has more than one entry, in cell form 
+        if iscell(parameters.time_ranges)
+           
+            parameters.segmented_timeseries = cell(size(parameters.time_ranges));
 
-    % For each mouse 
-    for mousei=1:size(mice_all,2)
-        mouse=mice_all(mousei).name;
-      
-        % For each day
-        for dayi=1:size(mice_all(mousei).days, 2)
-            
-            % Get the day name.
-            day=mice_all(mousei).days(dayi).name; 
-             
-            % Display mouse & day
-            disp(['mouse ' mouse ', day ' day]);
-            
-            % Create data input directory and cleaner output directory. 
-            dir_in_segment = CreateFileStrings(parameters.dir_in_segment_base, mouse, day, [], [], false);
-            dir_in_data = CreateFileStrings(parameters.dir_in_data_base, mouse, day, [], [], false);
-            parameters.dir_in = dir_in_data;
-            dir_out= CreateFileStrings([parameters.dir_out_base], mouse, day, [], [], false);
-          
-            % Get the velocities stack list
-            [stackList]=GetStackList(mousei, dayi, parameters);
-            
-            % Only make an output folder if there's data.
-            if ~isempty(stackList.numberList)
-                mkdir(dir_out);
+            for celli = 1:numel(parameters.time_ranges)
+                if ~isempty(parameters.time_ranges{celli})
+                    parameters.segmented_timeseries{celli} = SubSegmenter(parameters.time_ranges{celli}, parameters.timeseries, parameters.segmentDim, parameters.concatDim, celli); 
+    
+                end
             end 
-            
-            % For each stack, 
-            for stacki= 1:size(stackList.filenames,1)
-                
-                % Get the stack number and filename for the stack.
-                stack_number = stackList.numberList(stacki, :);
-                filename = stackList.filenames(stacki, :);
-                
-                % Load the timeseries stack. 
-                load([dir_in_data filename]);
-                
-                % Change the variable name of the timeseries data to
-                % something generic. 
-                eval(['Timeseries= ' input_data_variable ';']);
-                
-                % Get the filename of corresponding behavior segments.
-                segment_filename = CreateFileStrings(input_segment_name,[], [], stack_number, [], false);
-                
-                % load corresponding behavior segments. 
-                load([dir_in_segment segment_filename]); 
-                
-                % For each period,
-                for periodi = 1:size(periods_all,1)
-                    period = periods_all{periodi};
-                    
-                    % Make an empty matrix. 
-                    segmented_data = []; 
-                    
-                    try 
-                    % Get relevant segment variable name
-                    variable_name = CreateFileStrings(input_segment_variable,[], [], [], period, false);
-                    
-                    % Change segment ranges name to something generic. 
-                    eval(['segment_ranges = ' variable_name ';']);
-                    
-                    % Take the ranges using a flexible number of dimensions
-                    % C is a holder of as many ':' indices as we need.
-                    C = repmat({':'},1, ndims(timeseries));
-                   
-                    % Do the next part only if user said to use a strict 
-                    % time window AND they want to use full offsets
-                  
-                    if isfield(parameters, 'use_set_window') && parameters.use_set_window && isfield(parameters, 'full_transition_flag') && parameters.full_transition_flag
-                    
-                        % Change the time window to look at if it's a full
-                        % transition.
-                        if strcmp (period, 'full_onset') | strcmp(period, 'full_offset')
-                            time_window_use = (parameters.time_window_seconds + parameters.full_transition_extra_time)*2;
-                        else
-                            time_window_use = parameters.time_window_seconds;
-                        end 
-                    end
-                    
-                    % If no segment ranges, 
-                    if isempty(segment_ranges)
-                        % Do nothing.
-                    else
-                    
-                        % For each instance 
-                        for instancei = 1 : size(segment_ranges, 1)
-                            
-                            % Convert the ranges to time points. 
-                            all_ranges = segment_ranges(instancei,1):segment_ranges(instancei,2);
-                            
-                            % If user says so, get only the number of points 
-                            % that fit into the time window.
-                            if isfield(parameters, 'use_set_window') && parameters.use_set_window
-                                C(segmentDim) = {all_ranges(1: parameters.fps * time_window_use )};  %This is our index into timeseries.  
-                           
-                            % Otherwise, add the ranges normally
-                            else
-                                 C(segmentDim) = {all_ranges}; 
-                            end 
-                           
-                            % Try to concatenate; if one doesn't fit the
-                            % dimensions, skip it for now.
-                            try 
-                                segmented_data =cat(concatDim, segmented_data, Timeseries(C{:})); 
-                            catch 
-                                disp(['Dimension error in stack ' stack_number ', period ' period]);
-                                continue
-                            end 
-                        end
-                    end 
-                    % Get the output variable name
-                    output_variable_name = CreateFileStrings(output_variable,[], [], [], period, false);
-                    
-                    % Convert segmented data to the desired variable name
-                    eval([output_variable_name ' = segmented_data;']);
-                    catch 
-                        continue 
-                    end
-                end 
-                
-                % Get the right names for saving per stack. 
-                variable_searching_name = CreateFileStrings(output_variable,[], [], [], [], true);
-                saving_filename = CreateFileStrings(output_filename,[], [], stack_number, [], false);
-                
-                % Save per stack. 
-                save([dir_out saving_filename], variable_searching_name); 
+
+        else 
+            if ~isempty(parameters.time_ranges)
+                parameters.segmented_timeseries = SubSegmenter(parameters.time_ranges); 
+            else
+                parameters.segmented_timeseries = [];
             end
+        end 
+
+
+        % make this it's own sub-function
+end 
+
+function [segmented_timeseries] = SubSegmenter(time_ranges, timeseries, segmentDim, concatDim, celli)
+        
+         % Make an empty matrix. 
+        segmented_data = []; 
+        
+
+        % Take the ranges using a flexible number of dimensions
+        % C is a holder of as many ':' indices as we need.
+        C = repmat({':'},1, ndims(timeseries));
+
+         % For each instance 
+        for instancei = 1 : size(time_ranges, 1)
+            
+            % Convert the ranges to time points. 
+            all_ranges = time_ranges(instancei,1):time_ranges(instancei,2);
+          
+            % Put ranges into list of dimensions.
+            C(segmentDim) = {all_ranges}; 
+       
+           
+            % Try to concatenate; if one doesn't fit the
+            % dimensions, skip it for now.
+            try 
+                segmented_data =cat(concatDim, segmented_data, timeseries(C{:})); 
+
+                 % Put into output 
+                 segmented_timeseries = segmented_data; 
+            catch 
+                disp(['Dimension error in ' num2str(celli)]);
+                continue
+            end
+           
         end
-    end
+
+       
 end 
