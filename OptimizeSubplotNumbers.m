f78e4906e58f0b442aa67589248ceae7ae02b75b
Sarah West <west0883@umn.edu>
2022-05-26 09:27:42 -0500

    Case for only 1 plot number.

--- OptimizeSubplotNumbers.m
+++ OptimizeSubplotNumbers.m
@@ -13,30 +13,38 @@ function [subplot_rows, subplot_columns] = OptimizeSubplotNumbers(number_of_plot
         ratio = 2/3; 
     end 
     
-    % Do the math for the columns (unrounded)
-    columns = (1/ratio * number_of_plots) ^.5;
-    
-    % Find number of columns by rounding up to nearest integer (favors more
-    % columns than rows). 
-    subplot_columns = ceil(columns);
+    % If there is only one plot, skip all this and make just 1 subplot.
+    if number_of_plots == 1
+        subplot_rows = 1;
+        subplot_columns = 1;
 
-    % Find number of rows from unrounded columns number, then round down to
-    % nearest integer (favors more columns than rows).
-    subplot_rows = floor(ratio * columns);
+    else
 
-    % Make sure the optimization worked. If not, increase the number of
-    % rows (usually looks weird if you try increasing columns here), or if that isn't enough, increase the number of columns
-    while subplot_columns * subplot_rows < number_of_plots
+        % Do the math for the columns (unrounded)
+        columns = (1/ratio * number_of_plots) ^.5;
         
-        % Try increasing number of rows
-        if subplot_columns * (subplot_rows + 1) >= number_of_plots
-            subplot_rows = subplot_rows +1; 
-
-        else
-            % If that doesn't work, try also increasing number of columns & try
-            % again.
-            subplot_rows = subplot_rows + 1; 
-            subplot_columns = subplot_columns + 1; 
+        % Find number of columns by rounding up to nearest integer (favors more
+        % columns than rows). 
+        subplot_columns = ceil(columns);
+    
+        % Find number of rows from unrounded columns number, then round down to
+        % nearest integer (favors more columns than rows).
+        subplot_rows = floor(ratio * columns);
+    
+        % Make sure the optimization worked. If not, increase the number of
+        % rows (usually looks weird if you try increasing columns here), or if that isn't enough, increase the number of columns
+        while subplot_columns * subplot_rows < number_of_plots
+            
+            % Try increasing number of rows
+            if subplot_columns * (subplot_rows + 1) >= number_of_plots
+                subplot_rows = subplot_rows +1; 
+    
+            else
+                % If that doesn't work, try also increasing number of columns & try
+                % again.
+                subplot_rows = subplot_rows + 1; 
+                subplot_columns = subplot_columns + 1; 
+            end
         end
     end
 end 
