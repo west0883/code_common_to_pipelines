347c58eab8e0a9f07a819ca9c94294f75059f345
Sarah West <west0883@umn.edu>
2022-05-26 09:24:36 -0500

    Trying to fix issues with stack numbers if the number is a folder name, not in the file name.

--- CreateFileStrings.m
+++ CreateFileStrings.m
@@ -22,48 +22,50 @@ function [file_string]=CreateFileStrings(file_format_cell, mouse, day, stack_num
     
     % See if there is an entry for mouse number, find where it's located
     mouse_index=find(strcmp(file_format_cell,'mouse number'));
-     
+    
+    % If not empty
     if isempty(mouse_index)==0
          
-         % If we're creating a file string for searching directories, put a * in this place.
-         if searching
+         % If there is, make sure the mouse entry isn't empty 
+         if isempty(mouse)==0
+
+             % Put the mouse number in place of the mouse number tag
+             file_format_output_cell(mouse_index)={mouse}; 
+
+         % If the mouse input is empty, and you're searching
+         elseif searching
+            % We're creating a file string for searching directories, put a * in this place.
             file_format_output_cell(mouse_index)={'*'}; 
-         % If we're not creating this for searching,
+
+         % If mouse input is empty, but you're not searching throw an error
          else 
-             % If there is, make sure the mouse entry isn't empty 
-             if isempty(mouse)==0
-
-                 % Put the mouse number in place of the mouse number tag
-                 file_format_output_cell(mouse_index)={mouse}; 
-
-             % If the mouse input is empty, throw an error
-             else 
-                 error('no mouse number was given'); 
-             end 
+             error('no mouse number was given'); 
          end
-     end 
+         
+    end
      
     % See if there is an entry for day, find where it's located 
      day_index=find(strcmp(file_format_cell,'day'));
      
      if isempty(day_index)==0
+          
+         % If there is, make sure the day entry isn't empty 
+         if isempty(day)==0
 
-         % If we're creating a file string for searching directories, put a * in this place.
-         if searching
+             % Put the day number in place of the day number tag
+             file_format_output_cell(day_index)={day}; 
+         
+         % If it's empty, but we're searching 
+         elseif searching
+             
+            % We're creating a file string for searching directories, put a * in this place.
             file_format_output_cell(day_index)={'*'}; 
          
-         % If we're not creating this for searching,
+         % If it's empty & we're not creating this for searching, throw an
+         % error
          else 
-             % If there is, make sure the day entry isn't empty 
-             if isempty(day)==0
-
-                 % Put the mouse number in place of the mouse number tag
-                 file_format_output_cell(day_index)={day}; 
-
-             % If the mouse input is empty, throw an error
-             else 
-                 error('no day was given'); 
-             end 
+            error('no day was given'); 
+             
          end
      end 
      
@@ -71,48 +73,51 @@ function [file_string]=CreateFileStrings(file_format_cell, mouse, day, stack_num
      stack_index=find(strcmp(file_format_cell,'stack number'));
      
      if isempty(stack_index)==0
+         
          % If we're creating a file string for searching directories, put a * in this place.
-         if searching
-            file_format_output_cell(stack_index)={'*'}; 
+         file_format_output_cell(stack_index)={'*'};  
 
-            % If we're not creating this for searching,
-         else 
+         % If there is, make sure the stack number entry isn't empty 
+         if isempty(stack_number)==0
 
-             % If there is, make sure the stack number entry isn't empty 
-             if isempty(stack_number)==0
+             % Put the mouse number in place of the mouse number tag
+             file_format_output_cell(stack_index)={stack_number}; 
 
-                 % Put the mouse number in place of the mouse number tag
-                 file_format_output_cell(stack_index)={stack_number}; 
+         % If the stack input is empty and we're searching
+         elseif searching
 
-             % If the stack input is empty, throw an error
-             else 
-                 error('no stack number was given'); 
-             end 
-         end
+            % We're creating a file string for searching directories, put a * in this place.
+            file_format_output_cell(day_index)={'*'}; 
+
+         % If the stack is empty, but we're not searching
+         else
+             error('no stack number was given'); 
+         end 
+        
      end 
      
      % See if there is an entry for period, find where it is 
      period_index=find(strcmp(file_format_cell,'period name'));
      
      if isempty(period_index)==0
-         % If we're creating a file string for searching directories, put a * in this place.
-         if searching
-            file_format_output_cell(period_index)={'*'}; 
+    
+         % If there is, make sure the stack number entry isn't empty 
+         if isempty(period)==0
 
-            % If we're not creating this for searching,
+             % Put the mouse number in place of the mouse number tag
+             file_format_output_cell(period_index)={period}; 
+
+         % If the period input is empty and we're searching, 
+         elseif searching
+            
+             % We're creating a file string for searching directories, put a * in this place.
+             file_format_output_cell(period_index)={'*'}; 
+         
+         % If period input is empty, but we're not searching, throw an
+         % error
          else 
-
-             % If there is, make sure the stack number entry isn't empty 
-             if isempty(period)==0
-
-                 % Put the mouse number in place of the mouse number tag
-                 file_format_output_cell(period_index)={period}; 
-
-             % If the stack input is empty, throw an error
-             else 
-                 error('no period name was given'); 
-             end 
-         end
+             error('no period name was given'); 
+         end 
      end 
     
     % Now concatenate everything into a single string.
