ef936de5748027b861e21aeb8c3412ba5d90f00a
Sarah West <west0883@umn.edu>
2022-05-26 09:27:17 -0500

    Function to subtract data with RunAnalysis.

---
+++ SubtractData.m
@@ -0,0 +1,9 @@
+% SubtractData.m
+% Sarah West
+% 4/22/22
+
+function [parameters] = SubtractData(parameters)
+
+    parameters.data_subtracted = parameters.subtract_from_this - parameters.subtract_this;
+
+end 
