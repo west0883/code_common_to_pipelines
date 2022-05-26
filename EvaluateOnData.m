4f63509037aebc63fb0148f6b95d2bdbf5dc4458
Sarah West <west0883@umn.edu>
2022-05-26 09:27:32 -0500

    Runs any command with 'eval' through RunAnalysis.

---
+++ EvaluateOnData.m
@@ -0,0 +1,22 @@
+% EvaluateOnData.m
+% Sarah West
+% 5/10/22
+
+% A function that allows you to apply any expression to data using
+% RunAnalysis.
+
+function [parameters] = EvaluateOnData(parameters)
+    
+    % Display progress message to user.
+    MessageToUser('Evaluating on ', parameters); 
+
+    % Make evaluation string
+    evaluation_string = CreateStrings(parameters.evaluation_instructions, parameters.keywords, parameters.values);
+    
+    % Calculate
+    eval(['data_evaluated = ' evaluation_string ';']);
+
+    % Put into output.
+    parameters.data_evaluated = data_evaluated;
+
+end 
