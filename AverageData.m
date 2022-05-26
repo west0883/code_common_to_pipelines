f5e67b4926017b336070fc86befbe9a9aab28bef
Sarah West <west0883@umn.edu>
2022-05-26 09:26:49 -0500

    Averages data.

---
+++ AverageData.m
@@ -0,0 +1,23 @@
+% AverageData.m
+% Sarah West
+% 4/11/22
+
+function [parameters] = AverageData(parameters)
+
+    % If there's a "values" field from RunAnalysis, print updating message
+    % for user. 
+    if isfield(parameters, 'values')
+        message = ['Averaging '];
+        for dispi = 1:numel(parameters.values)/2
+           message = [message ', ' parameters.values{dispi}];
+        end
+        disp(message); 
+    end
+
+    %  Take the mean
+    parameters.average = mean(parameters.data, parameters.averageDim, 'omitnan'); 
+
+    % Take the standard deviation
+    parameters.std_dev = std(parameters.data, 0, parameters.averageDim, 'omitnan'); 
+
+end 
