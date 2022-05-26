42426576767db726efd284aaaf2f0c1f93cf0c10
Sarah West <west0883@umn.edu>
2022-05-26 09:27:29 -0500

    Add option to concatenate cell arrays, instead of only to within cell arrays.

--- ConcatenateData.m
+++ ConcatenateData.m
@@ -18,9 +18,28 @@ function [parameters] = ConcatenateData(parameters)
     
     % Display progress message to user.
     MessageToUser('Concatenating ', parameters);
+    
+    % If parameters.data has only one entry (no cells/not a cell array), or if user said to
+    % concatenated across (instead of within) cells. 
+    if ~iscell(parameters.data) || (isfield(parameters, 'concatenate_across_cells') && parameters.concatenate_across_cells)
+        % Make an empty variable for celli to determine display
+        % message (just puts an empty place in the message)
+        celli = []; 
+
+        % If concatenated_data doesn't exist yet, create it as empty
+        % array.
+        if ~isfield(parameters, 'concatenated_data')
+            parameters.concatenated_data = [];
+        end
+
+        % If data to concatenate isn't empty, add it to the
+        % concatenated data 
+        if ~isempty(parameters.data)
+            parameters.concatenated_data = SubConcatenateData(parameters.concatenated_data, parameters.data, parameters.concatDim, celli); 
+        end
 
     % If parameters.data has more than one entry, in cell form 
-    if iscell(parameters.data)
+    else iscell(parameters.data) 
        
         % If concatenated_data doesn't exist yet, create it as empty
         % cell array with same size as number of cells in data.
@@ -35,23 +54,6 @@ function [parameters] = ConcatenateData(parameters)
                 parameters.concatenated_data{celli} = SubConcatenateData(parameters.concatenated_data{celli}, parameters.data{celli}, parameters.concatDim, celli); 
 
             end
-        end 
-
-    else 
-        % Make an empty variable for celli to determine display
-        % message.
-        celli = []; 
-
-        % If concatenated_data doesn't exist yet, create it as empty
-        % array.
-        if ~isfield(parameters, 'concatenated_data')
-            parameters.concatenated_data = [];
-        end
-
-        % If data to concatenate isn't empty, add it to the
-        % concatenated data 
-        if ~isempty(parameters.data)
-            parameters.concatenated_data = SubConcatenateData(parameters.concatenated_data, parameters.data, parameters.concatDim, celli); 
         end
     end 
 end
