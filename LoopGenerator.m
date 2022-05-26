b65a09385ce46a5d5bf4bfed2ea399c7256fb39f
Sarah West <west0883@umn.edu>
2022-05-26 09:25:09 -0500

    Initial commit. Code to abstract levels of data loops for lab.

---
+++ LoopGenerator.m
@@ -0,0 +1,7 @@
+% LoopGenerator.m
+% Sarah West
+% 2/25/22
+
+% Creates list of data variables to cycle through. Is meant to make lab
+% code abstractable across users with different data division levels.
+
