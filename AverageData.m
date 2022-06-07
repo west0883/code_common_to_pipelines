ee05a40371385a73ef6b4af262a6b77efec6c909
Sarah West <west0883@umn.edu>
2022-06-06 19:45:33 -0500

    Option not to remove trailing dimensions.

--- AverageData.m
+++ AverageData.m
@@ -7,10 +7,19 @@ function [parameters] = AverageData(parameters)
     % Display progress message to user.
     MessageToUser('Averaging ', parameters);
 
-    %  Take the mean
-    parameters.average = squeeze(mean(parameters.data, parameters.averageDim, 'omitnan')); 
-
-    % Take the standard deviation
-    parameters.std_dev = squeeze(std(parameters.data, 0, parameters.averageDim, 'omitnan')); 
+    % if user says not to squee the data, don't
+    if isfield(parameters, 'useSqueeze') && ~parameters.useSqueeze
+        %  Take the mean
+        parameters.average = mean(parameters.data, parameters.averageDim, 'omitnan'); 
+    
+        % Take the standard deviation
+        parameters.std_dev = std(parameters.data, 0, parameters.averageDim, 'omitnan'); 
+    else
+         %  Take the mean
+        parameters.average = squeeze(mean(parameters.data, parameters.averageDim, 'omitnan')); 
+    
+        % Take the standard deviation
+        parameters.std_dev = squeeze(std(parameters.data, 0, parameters.averageDim, 'omitnan')); 
+    end
 
 end 
