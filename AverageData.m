be743d70d04dbb29a49cbb1c3dfe152fe214c168
Sarah West <west0883@umn.edu>
2022-06-24 10:04:45 -0500

    Option to put in same file or not.

--- AverageData.m
+++ AverageData.m
@@ -7,19 +7,28 @@ function [parameters] = AverageData(parameters)
     % Display progress message to user.
     MessageToUser('Averaging ', parameters);
 
-    % if user says not to squee the data, don't
+    % if user says not to squeeze the data, don't
     if isfield(parameters, 'useSqueeze') && ~parameters.useSqueeze
         %  Take the mean
-        parameters.average = mean(parameters.data, parameters.averageDim, 'omitnan'); 
+        average = mean(parameters.data, parameters.averageDim, 'omitnan'); 
     
         % Take the standard deviation
-        parameters.std_dev = std(parameters.data, 0, parameters.averageDim, 'omitnan'); 
+        std_dev = std(parameters.data, 0, parameters.averageDim, 'omitnan'); 
     else
          %  Take the mean
-        parameters.average = squeeze(mean(parameters.data, parameters.averageDim, 'omitnan')); 
+        average = squeeze(mean(parameters.data, parameters.averageDim, 'omitnan')); 
     
         % Take the standard deviation
-        parameters.std_dev = squeeze(std(parameters.data, 0, parameters.averageDim, 'omitnan')); 
+        std_dev = squeeze(std(parameters.data, 0, parameters.averageDim, 'omitnan')); 
+    end
+
+    % If user says to put the average & std_dev in the same file,
+    if isfield(parameters, 'average_and_std_together') && parameters.average_and_std_together
+       parameters.average = [average, std_dev];
+    else
+        % Put them in different places/files
+       parameters.average = average;
+       parameters.std_dev = std_dev;
     end
 
 end 
