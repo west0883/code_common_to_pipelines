e3bd1ae756c78a84c5e3f58e38002b2cb7634335
Sarah West <west0883@umn.edu>
2022-07-11 11:32:50 -0500

    Option for using normal distribution fit plus t-test or not.

--- SignificanceCalculation.m
+++ SignificanceCalculation.m
@@ -25,22 +25,54 @@ function [parameters] = SignificanceCalculation(parameters)
 
     % Find a p value by fitting a normal distibution to the null data & 
     % calculating the cumulative distribution function at the "real" sample values. 
-    significance.pvals_raw = normcdf(parameters.testing_values, mean(parameters.null_distribution, parameters.shufflesDim, 'omitnan'), ...
-        std(parameters.null_distribution,[], parameters.shufflesDim, 'omitnan'));  
 
-    % If user wants you to find the significance with the p-values
-    if isfield(parameters,'find_significance') && parameters.find_significance 
+    % If user doesn't want to use a normal distribution,
+    if isfield(parameters, 'useNormalDistribution')  && ~parameters.useNormalDistribution
 
-        % Default is 2-tailed
-        % If  2-tailed
+        % Divide alpha value in half (to get increase or decrease)
+        alpha = parameters.alphaValue ./ 2;
+
+        % (can't get out p-values from this, exactly)
+        % Sort null distribution data
+        null_distribution_sorted = sort(parameters.null_distribution, 2); 
+
+        increase_threshold_element_index = round(size(null_distribution_sorted,2) * (1 - alpha));
+        decrease_threshold_element_index = round(size(null_distribution_sorted,2) * alpha); 
+
+        % threshold values. 
+        increase_threshold_values = null_distribution_sorted(:, increase_threshold_element_index);
+        decrease_threshold_values = null_distribution_sorted(:, decrease_threshold_element_index);
+
+        % If the sizes of parameters.test_values & the threshold matrices
+        % don't match, transpose the threshold matrices. 
+        if ~isequal([size(parameters.test_values)], [size(increase_threshold_values)])
+
+            increase_threshold_values = increase_threshold_values';
+            decrease_threshold_values = decrease_threshold_values';
+        end
+        
+        % Calculate significance for increase & decrease.
+        significance.increase = parameters.test_values > increase_threshold_values;
+        significance.decrease = parameters.test_values < decrease_threshold_values;
+
+        % All significant results.
+        significance.all = significance.increase | significance.decrease;
+
+    % Otherwise, use a normal distribution
+    else
+       [~, significance.pvals_raw] = ztest(parameters.test_values, mean(parameters.null_distribution, parameters.shufflesDim, 'omitnan'), ...
+            std(parameters.null_distribution,[], parameters.shufflesDim, 'omitnan'));  
+
+       % Default is 2-tailed
+       % If  2-tailed
        % if ~isfield(parameters, 'twoTailed') || parameters.twoTailed
  
             % Divide alpha value in half (to get increase or decrease)
-            alpha = parameters.alphaValue / 2;
+            alpha = parameters.alphaValue ./ 2;
 
             % Calculate significance for increase & decrease.
-            significance.increase = parameters.pvals_raw > 1 - alpha; 
-            significance.decrease = parameters.pvals_raw < 1 - alpha; 
+            significance.increase = significance.pvals_raw > (1 - alpha); 
+            significance.decrease = significance.pvals_raw < alpha; 
 
             % All significant results.
             significance.all = significance.increase | significance.decrease;
@@ -50,9 +82,9 @@ function [parameters] = SignificanceCalculation(parameters)
 %             significance.
 % 
 %         end
+    end
 
-        % Put significance into parameters structure.
-        parameters.significance = significance;
-    end 
+    % Put significance into parameters structure.
+    parameters.significance = significance;
 
 end 
