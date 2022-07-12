00a261896e64c1452af2886cc2030fc70a87d786
Sarah West <west0883@umn.edu>
2022-07-12 17:54:38 -0500

    Option to find & remove outliers.

--- AverageData.m
+++ AverageData.m
@@ -7,19 +7,30 @@ function [parameters] = AverageData(parameters)
     % Display progress message to user.
     MessageToUser('Averaging ', parameters);
 
+    % Pull out data.
+    data = parameters.data; 
+
+    % If user says to remove outliers
+    if isfield(parameters, 'removeOutliers') && parameters.removeOutliers
+
+        % Remove outliers along averageDim, replace with NaNs.
+        outliers = isoutlier(data, parameters.averageDim);
+        data(outliers) = NaN;
+    end 
+
     % if user says not to squeeze the data, don't
     if isfield(parameters, 'useSqueeze') && ~parameters.useSqueeze
         %  Take the mean
-        average = mean(parameters.data, parameters.averageDim, 'omitnan'); 
+        average = mean(data, parameters.averageDim, 'omitnan'); 
     
         % Take the standard deviation
-        std_dev = std(parameters.data, 0, parameters.averageDim, 'omitnan'); 
+        std_dev = std(data, 0, parameters.averageDim, 'omitnan'); 
     else
          %  Take the mean
-        average = squeeze(mean(parameters.data, parameters.averageDim, 'omitnan')); 
+        average = squeeze(mean(data, parameters.averageDim, 'omitnan')); 
     
         % Take the standard deviation
-        std_dev = squeeze(std(parameters.data, 0, parameters.averageDim, 'omitnan')); 
+        std_dev = squeeze(std(data, 0, parameters.averageDim, 'omitnan')); 
     end
 
     % If user says to put the average & std_dev in the same file,
