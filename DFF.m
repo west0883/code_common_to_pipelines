e831f8048fe9a66e930f44d4cccdd7650f4a9f4a
Sarah West <west0883@umn.edu>
2022-12-02 14:43:25 -0600

    function that finds delta F over F

---
+++ DFF.m
@@ -0,0 +1,19 @@
+% DFF.m
+% Sarah West
+% 12/2/22
+% Function that finds the delta F/F, given a set of fluorescence values and
+% a mean. Is run by RunAnalysis
+
+% Inputs: 
+% parameters.data -- the fluorescence values to convert into percents
+% parameters.fluorescence_mean -- the mean fluorescence, should work with
+% the dimensions of parameters.data
+
+% Outputs:
+% parameters.DFF -- the fluorescence values converted to percents
+
+function [parameters] = DFF(parameters)
+
+    parameters.DFF = 100 * ((parameters.values + parameters.fluorescence_mean)./parameters.fluorescence_mean) - 100; 
+
+end 
